95ea8197f89177a94a017b9008a55bce000151f6
rmansurbek2197 <rmansurbek2197@gmail.com>
2026-04-18 23:29:06 +0500

    Initial commit

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
-n = int(input())
-while n > 0:
-    print(n % 10)
-    n = n // 10
+class MatnAjratish:
+    def __init__(self, matn):
+        self.matn = matn
+        self.unlilar = "aeiou"
+        self.undoshlar = "bcdfghjklmnpqrstvwxyz"
+        self.unli_harflar = []
+        self.undosh_harflar = []
+
+    def ajratish(self):
+        for harf in self.matn.lower():
+            if harf in self.unlilar:
+                self.unli_harflar.append(harf)
+            elif harf in self.undoshlar:
+                self.undosh_harflar.append(harf)
+
+    def chiqarish(self):
+        print("Unli harflar: ", self.unli_harflar)
+        print("Undosh harflar: ", self.undosh_harflar)
+
+
+matn = input("Matn kiriting: ")
+dastur = MatnAjratish(matn)
+dastur.ajratish()
+dastur.chiqarish()
